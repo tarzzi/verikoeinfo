2f6a1324a4c94f4531f16896cf2da6c3ede22953
Tarmo Urrio <5585433+tarzzi@users.noreply.github.com>
2021-04-19 00:03:25 +0300

    update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -2,5 +2,5 @@ import React from "react"
 
 export default function Header() {
   return <div className="center">
-    <h2>Verikoe.info - Tietoa verikokeista</h2></div>
+    <h2>Verikoe.info - Tietoa verikokeista yksinkertaisesti</h2></div>
 }
