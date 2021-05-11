e938b45443243be1da80a748398fe728ecc5dc1d
Tarmo <n4378@student.jamk.fi>
2021-05-12 01:05:07 +0300

    Lisätty divit

--- src/components/cTitle.js
+++ src/components/cTitle.js
@@ -2,9 +2,10 @@ import React, { Component }  from 'react';
 
 export default function Ctitle({children}){
   return (
+    <div>
     <h1 className='text-center text-gray-900 bg-red-200 font-bold py-3'>
       {children}
-    </h1>
+    </h1></div>
   )
 
 }
--- src/components/values.js
+++ src/components/values.js
@@ -2,9 +2,11 @@ import React, { Component }  from 'react';
 
 export default function Values({children}){
   return (
-    <p className='p-4'>
-      {children}
-    </p>
+    <div>
+      <p className='p-4'>
+        {children}
+      </p>
+    </div>
   )
 
 }
