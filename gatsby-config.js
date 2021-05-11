a82426166a71249c53c52f7536c0db4264fd760d
Tarmo <n4378@student.jamk.fi>
2021-05-11 23:57:45 +0300

    Muutettu favicon, muokattu ga-asetuksia. Poistettu työmaamerkit etusivulta.

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,8 @@ module.exports = {
       options: {
         pathToConfigModule: `./src/utils/typography`,
       },
-    }, `gatsby-plugin-postcss`,
+    },
+    `gatsby-plugin-postcss`,
       {
         resolve: `gatsby-plugin-google-analytics`,
         options: {
@@ -25,12 +26,6 @@ module.exports = {
           trackingId: "G-C9XRNM6GQY",
           // Defines where to place the tracking script - `true` in the head and `false` in the body
           head: true,
-          // Setting this parameter is optional
-          anonymize: true,
-          // Setting this parameter is also optional
-          respectDNT: true,
-          // Defers execution of google analytics script after page load
-          defer: false,
         },
       },
   ],
--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,6 @@ export default function Home() {
     </Helmet>
     <Header />
     <div className='text-center lg:text-3xl md:text-3xl sm:text-2xl py-3 my-1'>Valitse kokeen kategoria, niin näet tarkemmat tiedot</div>
-    <div className='text-center lg:text-xl md:text-xl sm:text-md py-3 my-1 font-bold'> 🚧 Sivusto on työn alla ja harrastepohjainen 🚧 <br /></div>
     <div className="grid lg:grid-cols-3 lg:text-3xl md:grid-cols-3 md:text-2xl sm:grid-cols-1 sm:text-2xl text-3xl grid-cols-1 justify-center items-center transition-all duration-100 bg-white">
         <Link to='/kilpirauhanen' className="border-2 border-gray-400 bg-gray-200 hover:bg-gray-700 hover:text-white py-8 lg:py-16 md:py-16 sm:py-8 text-center ">Kilpirauhanen</Link>
         <Link to='/maksa'         className="border-2 border-gray-400 bg-gray-200 hover:bg-gray-700 hover:text-white py-8 lg:py-16 md:py-16 sm:py-8 text-center ">Maksa</Link>
