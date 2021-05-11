faccfe6431e078a94a9eea193e73a479eccec633
Tarmo <n4378@student.jamk.fi>
2021-05-12 00:09:48 +0300

    lisätty tailwind.config tiedosto, muokattu alaotsikkoa

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       <script src={withPrefix('doorbell.js')} type='text/javascript' />
     </Helmet>
     <Header />
-    <div className='text-center lg:text-3xl md:text-3xl sm:text-2xl py-3 my-1'>Valitse kokeen kategoria, niin näet tarkemmat tiedot</div>
+    <div className='text-center lg:text-3xl md:text-3xl sm:text-2xl py-3 my-1'>Valitse verikokeen kategoria, niin näet tarkemmat tiedot</div>
     <div className="grid lg:grid-cols-3 lg:text-3xl md:grid-cols-3 md:text-2xl sm:grid-cols-1 sm:text-2xl text-3xl grid-cols-1 justify-center items-center transition-all duration-100 bg-white">
         <Link to='/kilpirauhanen' className="border-2 border-gray-400 bg-gray-200 hover:bg-gray-700 hover:text-white py-8 lg:py-16 md:py-16 sm:py-8 text-center ">Kilpirauhanen</Link>
         <Link to='/maksa'         className="border-2 border-gray-400 bg-gray-200 hover:bg-gray-700 hover:text-white py-8 lg:py-16 md:py-16 sm:py-8 text-center ">Maksa</Link>
