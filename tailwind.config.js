28beabcdcdd30a765607a07d957c77ed5c0f7a79
Tarmo <n4378@student.jamk.fi>
2021-05-12 00:34:50 +0300

    Tailwindin purge failaa buildin purgen yhteydessä, testinä disable

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}"],
+  purge: {
+    enabled: false,
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
