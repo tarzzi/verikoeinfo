c622b14fcd5f62073ec23b15f4372a4b42fa41cd
Tarmo Urrio <5585433+tarzzi@users.noreply.github.com>
2021-11-25 23:37:33 +0200

    Update background to pass WCAG AA

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function Footer() {
   return(
-  <div className="text-center mt-5 mb-2 ">
-    <a href='https://urrio.fi' className='text-gray-400 hover:text-red-700'><p>Made by:  Tarmo</p></a>
+  <div className="text-center mt-5 mb-2">
+    <a href='https://urrio.fi' className='text-gray-800 hover:text-red-700'><p>Made by:  Tarmo</p></a>
       <span className='small italic text-xs'>päivitetty 11.05.2021</span>
     <p className='text-sm font-thin'>Sivusto käyttää keksejä kävijämäärän seurantaan</p>
   </div>
--- src/components/header.js
+++ src/components/header.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function Header() {
   return <div className="center" id='top'>
-    <h2 className='text-3xl text-center bg-red-400 py-8 text-white border-b-2 border-gray-500'>Veriarvot.info - Yleisimmät verikokeet ja viitearvot</h2>
+    <h2 className='text-3xl bg-red-600 text-center py-8 text-white border-b-1 border-gray-500'>Veriarvot.info - Yleisimmät verikokeet ja viitearvot</h2>
   </div>
 }
